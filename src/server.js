1a226baec1032e35c9841b4a5c1d180e2e7397a6
Paulo Victor Rios <victorios.ba@gmail.com>
2020-02-10 07:04:23 -0300

    Adiciona Procfile e .ENV

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const routes  = require('./routes');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-oqmyl.mongodb.net/semana09?retryWrites=true&w=majority'
+mongoose.connect(process.env.MONGO_URL
 , {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -24,5 +24,5 @@ app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
 

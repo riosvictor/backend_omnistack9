06eebc6e7f21f2c920e1aaf54c93cdea0be6aa81
Paulo Victor Rios <victorios.ba@gmail.com>
2020-02-11 12:41:49 -0300

    Remoção do trecho virtual

--- src/models/Spot.js
+++ src/models/Spot.js
@@ -15,8 +15,4 @@ const SpotSchema = new mongoose.Schema({
     }
 })
 
-SpotSchema.virtual('thumbnail_url').get(function() {
-    return `${this.thumbnail}`
-})
-
 module.exports = mongoose.model('Spot', SpotSchema);

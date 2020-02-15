a825f3a028cadba01c4da941e184ec46d6cb511e
Paulo Victor Rios <victorios.ba@gmail.com>
2020-02-15 06:42:29 -0300

    Novas funcionalidades para funcionamento em tempo real

--- src/server.js
+++ src/server.js
@@ -2,15 +2,34 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
+const socketio = require('socket.io');
+const http = require('http');
 
 const routes  = require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect(process.env.MONGO_URL
 , {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+});
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user_id } = socket.handshake.query;
+
+    connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
 });
 
 //GET - buscar info, POST - criar info, PUT - editar info, DELETE - deletar info
@@ -24,5 +43,5 @@ app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(process.env.PORT || 3333);
+server.listen(process.env.PORT || 3333);
 
